3a5cfe4c9a513c274db078b5f9506a92273956ec
Trevor Clarke <besnoid@gmail.com>
2020-02-19 15:39:20 -0800

    setup arb list overview

--- src/components/ArbitrageList.vue
+++ src/components/ArbitrageList.vue
@@ -1,48 +1,68 @@
 <template>
   <div id="arbitrage">
-    <Table :loading="loading" size="small" height="600" border :columns="columns" :data="allItems"></Table>
+    <Table row-key="id" :loading="loading" size="small" height="600" border :columns="columns" :data="allItems"></Table>
   </div>
 </template>
 
 <script>
 const columns = [
-  {
-      title: '#',
-      key: 'rank',
-      // fixed: 'left',
-      width: 50
-  },
+  // {
+  //     title: '#',
+  //     key: 'rank',
+  //     // fixed: 'left',
+  //     width: 50
+  // },
   {
       title: 'Asset',
       key: 'name',
+      tree: true,
       // fixed: 'left',
       // width: 100
   },
-  {
-      title: 'Symbol',
-      key: 'symbol',
-      width: 100
-  },
-  {
-      title: 'Price',
-      key: 'currentPrice',
-      // width: 130
-  },
-  {
-      title: '% Change',
-      key: 'changeInPrice',
-      // width: 100
-  },
-  {
-      title: 'Total Pairs',
-      key: 'totalPairs',
-      // width: 100
-  },
   // {
-  //     title: 'Pairs',
-  //     key: 'pairs',
+  //     title: 'Symbol',
+  //     key: 'symbol',
+  //     width: 100
+  // },
+  // {
+  //     title: 'Price',
+  //     key: 'currentPrice',
+  //     // width: 130
+  // },
+  // {
+  //     title: '% Change',
+  //     key: 'changeInPrice',
   //     // width: 100
   // },
+  // {
+  //     title: 'Total Pairs',
+  //     key: 'totalPairs',
+  //     // width: 100
+  // },
+  {
+      title: 'Pair',
+      key: 'pair',
+  },
+  {
+      title: 'Exchange',
+      key: 'exchange',
+  },
+  {
+      title: 'Spread',
+      key: 'spread',
+  },
+  {
+      title: 'Last',
+      key: 'last',
+  },
+  {
+      title: 'Bid',
+      key: 'bid',
+  },
+  {
+      title: 'Ask',
+      key: 'ask',
+  },
 ]
 
 export default {
@@ -56,6 +76,9 @@ export default {
     }
   },
   methods: {
+    getSpread(ticker) {
+      return Number(ticker.ask - ticker.bid).toFixed(4)
+    },
     associatePairData(arr) {
       const ex = this.exchanges
       if (!ex || ex.length <= 0) return arr
@@ -70,7 +93,8 @@ export default {
             // Associate base only
             if (p.split('_')[0] === a.symbol.toLowerCase()) {
               a.pairs = a.pairs || []
-              a.pairs.push(p)
+              // a.pairs.push({ pair: p, exchange: e })
+              if (!a.pairs.includes(p)) a.pairs.push(p)
             }
           })
         })
@@ -86,19 +110,62 @@ export default {
     },
     async getRankings() {
       const res = await this.$w3d.market.getRankings()
-      this.loading = false
+      // this.loading = false
       if (!res || !res.data) return
       const items = this.associatePairData(res.data)
-      console.log('getRankings', items)
-      this.allItems = items
+      // console.log('getRankings', items)
+      // this.allItems = items
+
+      // Now loop everything and find the tickers for each
+      this.getPrices(items.slice(0, 2))
+      // this.getPrices(items)
     },
-    async getPrices() {
-      const res = await this.$w3d.market.getRankings()
+    async getPrices(list) {
+      if (!list || list.length <= 0) return
+      const all = await list.map(async (l, idx) => {
+        l.children = []
+        l.id = `${l.rank}${idx}`
+        await l.pairs.forEach(async (p, pdx) => {
+          try {
+            const tickers = await this.$w3d.market.getTickers(p)
+            // console.log('ticker', tickers)
+
+            // l.children.push(ticker[p.exchange])
+            Object.keys(tickers).forEach((t, tdx) => {
+              l.children.push({
+                ...tickers[t],
+                id: `${l.rank}${idx}${pdx}${tdx}`,
+                name: l.name,
+                symbol: l.symbol,
+                pair: p,
+                exchange: t,
+                spread: this.getSpread(tickers[t]),
+              })
+            })
+          } catch(e) {
+            console.log('e', e)
+          }
+        })
+
+        // tmpItems[idx] = l
+        // this.allItems = tmpItems
+        return l
+      })
+
+      const updatedItems = await Promise.all(all)
+      console.log('updatedItems', updatedItems)
+      this.allItems = updatedItems
+      console.log('this.allItems', this.allItems)
+
+      // this.allItems = []
+      // this.allItems = updatedItems
       this.loading = false
-      if (!res || !res.data) return
-      const items = this.associatePairData(res.data)
-      console.log('getRankings', items)
-      this.allItems = items
+
+      // setTimeout(() => {
+      //   this.allItems = []
+      //   this.allItems = updatedItems
+      //   this.loading = false
+      // }, 3000)
     },
   },
 
--- src/pages/Arbitrage.vue
+++ src/pages/Arbitrage.vue
@@ -3,7 +3,7 @@
     <h1>Arbitrage</h1>
     <Row style="padding: 20px 0">
       <Col span="24">
-        <Card shadow>
+        <Card v-if="exchanges" shadow>
           <ArbitrageList :exchanges="exchanges"></ArbitrageList>
         </Card>
       </Col>
