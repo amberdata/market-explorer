b61c39c9d8293da4e6d67ca99f20b4e52700dc5c
Trevor Clarke <besnoid@gmail.com>
2020-02-19 12:11:37 -0800

    setup easier changing

--- src/components/ComparisonChart.vue
+++ src/components/ComparisonChart.vue
@@ -23,11 +23,6 @@
           <Select v-model="pairA.pairSelected" @on-change="refreshData()" style="width:110px">
             <Option v-for="pair in pairNamesA" :value="pair" :key="pair">{{ pair }}</Option>
           </Select>
-
-          <Select v-model="pairA.timeIntervalSelected" @on-change="refreshData()" style="width:110px">
-            <Option v-for="item in timeIntervals" :value="item" :key="item">{{ item }}</Option>
-          </Select>
-          <!-- <DatePicker type="daterange" :start-date="new Date()" placement="bottom-end" placeholder="Select date" style="width: 160px"></DatePicker> -->
         </div>
         <div class="form-line">
           <small>Asset 2:</small>
@@ -37,13 +32,11 @@
           <Select v-model="pairB.pairSelected" @on-change="refreshData()" style="width:110px">
             <Option v-for="pair in pairNamesB" :value="pair" :key="pair">{{ pair }}</Option>
           </Select>
-
-          <Select v-model="pairB.timeIntervalSelected" @on-change="refreshData()" style="width:110px">
-            <Option v-for="item in timeIntervals" :value="item" :key="item">{{ item }}</Option>
-          </Select>
-          <!-- <DatePicker type="daterange" :start-date="new Date()" placement="bottom-end" placeholder="Select date" style="width: 160px"></DatePicker> -->
         </div>
       </div>
+      <Select v-model="timeIntervalSelected" @on-change="refreshData()" style="width:110px">
+        <Option v-for="item in timeIntervals" :value="item" :key="item">{{ item }}</Option>
+      </Select>
       <Button type="info" v-on:click="refreshChart()">Refresh Chart</Button>
     </div>
     <div v-bind:id="chartName"></div>
@@ -93,7 +86,6 @@
           exchangeSelected: null,
           pairSelected: null,
           timeIntervals: ['days', 'hours', 'minutes'],
-          timeIntervalSelected: 'days',
           startDate: '...',
           endDate: '...',
         },
@@ -101,7 +93,6 @@
           exchangeSelected: null,
           pairSelected: null,
           timeIntervals: ['days', 'hours', 'minutes'],
-          timeIntervalSelected: 'days',
           startDate: '...',
           endDate: '...',
         },
@@ -149,40 +140,27 @@
         // if (this.startDate && this.startDate !== '...') options.startDate = this.startDate
         // if (this.endDate && this.endDate !== '...') options.endDate = this.endDate
         if (this.pairA.exchangeSelected) optionsA.exchange = this.pairA.exchangeSelected
-        if (this.pairA.timeIntervalSelected) optionsA.timeInterval = this.pairA.timeIntervalSelected
+        if (this.timeIntervalSelected) optionsA.timeInterval = this.timeIntervalSelected
         optionsA.endDate = +new Date()
         if (this.pairB.exchangeSelected) optionsB.exchange = this.pairB.exchangeSelected
-        if (this.pairB.timeIntervalSelected) optionsB.timeInterval = this.pairB.timeIntervalSelected
+        if (this.timeIntervalSelected) optionsB.timeInterval = this.timeIntervalSelected
         optionsB.endDate = +new Date()
 
         const ohlcvResA = await this.$w3d.market.getOhlcv(this.pairA.pairSelected, optionsA)
         const ohlcvDataA = optionsA.startDate || optionsA.endDate ? ohlcvResA.data[optionsA.exchange] : ohlcvResA[optionsA.exchange]
         const ohlcvResB = await this.$w3d.market.getOhlcv(this.pairB.pairSelected, optionsB)
         const ohlcvDataB = optionsB.startDate || optionsB.endDate ? ohlcvResB.data[optionsB.exchange] : ohlcvResB[optionsA.exchange]
-
-        // if (ohlcvDataA) {
-        //   this.chart.data = ohlcvDataA.map(x => ({ date: x[0], open: x[1], high: x[2], low: x[3], close: x[4], volume: x[5] }))
-        //   console.log('this.chart.data', this.chart.data)
-        // }
-        // if (ohlcvDataB) {
-        //   this.chart.data = ohlcvDataB.map(x => ({ date: x[0], open: x[1], high: x[2], low: x[3], close: x[4], volume: x[5] }))
-        // }
-
         const combinedData = []
-        console.log('!ohlcvDataA || !ohlcvDataB', !ohlcvDataA, !ohlcvDataB)
         if (!ohlcvDataA || !ohlcvDataB) return
 
         // combine both data sets into timestamp matched data array
         ohlcvDataA.forEach(oA => {
           ohlcvDataB.forEach(oB => {
-            // if (oA[0] === oB[0]) combinedData.push({ date: oA[0], a: oA[4], b: oB[4] })
             if (oA[0] === oB[0]) {
-              // console.log('oA[0] === oB[0]', oA[0] === oB[0], oA[0], oB[0])
               combinedData.push({ date: oA[0], dateB: oB[0], pairA: oA[4], labelA: 'A', pairB: oB[4], labelB: 'B' })
             }
           })
         })
-        console.log('combinedData', combinedData)
 
         this.chart.data = combinedData
       },
